86da7bc77904dedc3e1b9b9ff7ecf1b19e6b2e58
Kshitij Deshpande <kshitijdeshpande9325@gmail.com>
2024-02-12 22:27:31 +0530

    created form

---
+++ app.js
@@ -0,0 +1,13 @@
+const input = document.querySelectorAll(".contact-input");
+input.forEach(ipt => {
+  ipt.addEventListener("focus", () => {
+    ipt.parentNode.classList.add("focus");
+    ipt.parentNode.classList.add("not-empty");
+  })
+  ipt.addEventListener("blur", () => {
+    // if (ipt.value == "") {
+    //   ipt.parentNode.classList.remove("not-empty");
+    // }
+    ipt.parentNode.classList.remove("not-empty");
+  })
+});
