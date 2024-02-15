0efe69b4d7e5e7b021dd28682f8041fdc9443be9
Kshitij Deshpande <kshitijdeshpande9325@gmail.com>
2024-02-16 00:42:39 +0530

    form completed (not responsive)

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const input = document.querySelectorAll(".contact-input");
+const toggleBtn = document.querySelector(".theme-toggle");
+const allElements = document.querySelectorAll("*");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  allElements.forEach((el) => {
+    el.classList.add("transition");
+    setTimeout(() => {
+      el.classList.remove("transition");
+    }, 1000);
+  })
+})
+
 input.forEach(ipt => {
   ipt.addEventListener("focus", () => {
     ipt.parentNode.classList.add("focus");
